863db9bf93d2eeafb9e381efa082a9f0fafd2567
Tuukka Turto <tuukka.turto@oktaeder.net>
2018-07-11 21:36:08 +0300

    move widgets into separate folder

--- src/Widgets.hs
+++ src/Widgets.hs
@@ -11,22 +11,22 @@ import Report
 import MenuHelpers (toDisplayDate)
 
 systemDetails :: CollatedStarSystemReport -> WidgetFor site ()
-systemDetails systemReport = $(widgetFile "systemdetailsW")
+systemDetails systemReport = $(widgetFile "widgets/systemDetailsW")
 
 planetReportList :: Foldable t => t CollatedPlanetReport -> WidgetFor App ()
-planetReportList planetReports = $(widgetFile "planetlistW")
+planetReportList planetReports = $(widgetFile "widgets/planetListW")
 
 planetDetails :: CollatedPlanetReport -> WidgetFor App ()
-planetDetails planetReport = $(widgetFile "planetdetailsW")
+planetDetails planetReport = $(widgetFile "widgets/planetDetailsW")
 
 starReportList :: Foldable t => t CollatedStarReport -> WidgetFor site ()
-starReportList starReports = $(widgetFile "starListW")
+starReportList starReports = $(widgetFile "widgets/starListW")
 
 starLaneList :: Foldable t => t CollatedStarLaneReport -> WidgetFor App ()
-starLaneList starLaneReports = $(widgetFile "starlanesW")
+starLaneList starLaneReports = $(widgetFile "widgets/starlanesW")
 
 buildingList :: Foldable t => t CollatedBuildingReport -> WidgetFor site ()
-buildingList buildingReports = $(widgetFile "buildingListW")
+buildingList buildingReports = $(widgetFile "widgets/buildingListW")
 
 populationList :: Foldable t => t CollatedPopulationReport -> WidgetFor site ()
-populationList populationReports = $(widgetFile "widgets/populationlistW")
+populationList populationReports = $(widgetFile "widgets/populationListW")
