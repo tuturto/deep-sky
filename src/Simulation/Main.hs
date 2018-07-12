8bced99eb404ba8bafbc68d91b2944407e62dc0c
Tuukka Turto <tuukka.turto@oktaeder.net>
2018-07-12 10:28:41 +0300

    working with food

--- src/Simulation/Main.hs
+++ src/Simulation/Main.hs
@@ -13,6 +13,7 @@ module Simulation.Main where
 
 import Import
 import Simulation.Time
+import CustomTypes
 import System.Random
 
 -- | simulate a single step
@@ -32,9 +33,12 @@ handleFaction :: (BaseBackend backend ~ SqlBackend,
     Entity Faction -> ReaderT backend m ()
 handleFaction faction = do
     planets <- selectList [ PlanetOwnerId ==. Just (entityKey faction)] []
-    bios <- mapM getFoodRequirement $ map entityKey planets
-    let reqBio = foldr (+) 0 bios
-    _ <- update (entityKey faction) [ FactionBiologicals -=. reqBio ]
+    lReqBio <- mapM getFoodRequirement $ map entityKey planets
+    let reqBio = foldr (+) 0 lReqBio
+    lProdBio <- mapM getFoodProduction $ map entityKey planets
+    let prodBio = foldr (+) 0 lProdBio
+    let deltaBio = prodBio - reqBio
+    _ <- update (entityKey faction) [ FactionBiologicals +=. deltaBio ]
     return ()
 
 -- | calculate amount of food a given planet requires
@@ -43,11 +47,29 @@ getFoodRequirement :: (BaseBackend backend ~ SqlBackend, MonadIO m,
     Key Planet -> ReaderT backend m Int
 getFoodRequirement pid = do
     pop <- selectList [ PlanetPopulationPlanetId ==. pid ] []
-    let res = pFoodRequirement $ map entityVal pop
+    let res = foodRequirement $ map entityVal pop
+    return res
+
+-- | calculate amount of food a given planet produces
+getFoodProduction :: (BaseBackend backend ~ SqlBackend, MonadIO m,
+    PersistQueryRead backend) =>
+    Key Planet -> ReaderT backend m Int
+getFoodProduction pid = do
+    buildings <- selectList [ BuildingPlanetId ==. pid ] []
+    let res = foodProduction $ map entityVal buildings
     return res
 
 -- | calculate amount of food given population requires
-pFoodRequirement :: [PlanetPopulation] -> Int
-pFoodRequirement population = 
+foodRequirement :: [PlanetPopulation] -> Int
+foodRequirement population = 
     totalPopulation * 2
         where totalPopulation = foldr (\a b -> planetPopulationPopulation a + b) 0 population
+
+-- | calculate amount of food produced by group of buildings
+foodProduction :: [Building] -> Int
+foodProduction buildings =
+    foldr (+) 0 productions 
+        where productions = map prod buildings
+              prod x = case (buildingType x) of
+                            Farm -> 5
+                            _    -> 0
--- test/QC/Planets.hs
+++ test/QC/Planets.hs
@@ -3,23 +3,13 @@ module QC.Planets where
 
 import Test.QuickCheck.All
 import Test.QuickCheck.Arbitrary
-import Test.QuickCheck.Modifiers
 import Test.QuickCheck.Gen
 import Test.QuickCheck
-import Test.QuickCheck.Instances.Text
 
 import Database.Persist.Sql (toSqlKey)
 import Model
-import Simulation.Main (pFoodRequirement)
-
-instance Arbitrary Planet where
-    arbitrary = do
-        aName <- arbitrary
-        aPosition <- arbitrary
-        let aStarSystemId = toSqlKey 0
-        let aOwnerId = Nothing
-        aGravity <- arbitrary
-        return $ Planet aName (getPositive aPosition) aStarSystemId aOwnerId (getPositive aGravity)
+import CustomTypes
+import Simulation.Main (foodRequirement, foodProduction)
 
 singlePopulation :: Gen PlanetPopulation
 singlePopulation = do
@@ -38,13 +28,34 @@ allPopulations = do
     k <- arbitrary `suchThat` \x -> x >= 0
     vectorOf k singlePopulation
 
+singleBuildingOf :: BuildingType -> Gen Building
+singleBuildingOf bType = do
+    let aPlanetId = toSqlKey 0
+    aLevel <- choose (1, 5)
+    let aConstruction = 1.0
+    let aDamage = 0.0
+    return $ Building aPlanetId bType aLevel aConstruction aDamage
+
+someFarms :: Gen [Building]
+someFarms = do
+    k <- arbitrary `suchThat` \x -> x > 0
+    vectorOf k $ singleBuildingOf Farm
+
 prop_foodRequirement_for_positive_amount_of_population_is_more_than_zero :: Property
 prop_foodRequirement_for_positive_amount_of_population_is_more_than_zero  
-    = forAll positivePopulation $ \res -> pFoodRequirement res  > 0
+    = forAll positivePopulation $ \x -> foodRequirement x  > 0
 
 prop_foodRequirement_for_all_populations_is_non_negative :: Property
 prop_foodRequirement_for_all_populations_is_non_negative  
-    = forAll allPopulations $ \res -> pFoodRequirement res  >= 0
+    = forAll allPopulations $ \x -> foodRequirement x  >= 0
+
+prop_foodProduction_for_farms_is_non_negative :: Property
+prop_foodProduction_for_farms_is_non_negative
+    = forAll someFarms $ \x -> foodProduction x > 0
+
+prop_food_production_for_farms_is_equal_or_greater_than_their_amount :: Property
+prop_food_production_for_farms_is_equal_or_greater_than_their_amount
+    = forAll someFarms $ \x -> foodProduction x > length x
 
 --------------------------
 return []
