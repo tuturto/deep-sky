d605c07ac43fd017352bcf1f83215dbc1328e586
Tuukka Turto <tuukka.turto@oktaeder.net>
2018-07-12 17:59:26 +0300

    working on observations

--- src/Simulation/Main.hs
+++ src/Simulation/Main.hs
@@ -14,8 +14,8 @@ module Simulation.Main where
 import Import
 import Simulation.Time
 import Simulation.Food (handleFactionFood)
-import CustomTypes
 import System.Random
+import MenuHelpers
 
 -- | simulate a single step
 processTurn :: (BaseBackend backend ~ SqlBackend,
@@ -33,5 +33,48 @@ handleFaction :: (BaseBackend backend ~ SqlBackend,
     PersistStoreWrite backend, MonadIO m, PersistQueryRead backend) =>
     Entity Faction -> ReaderT backend m ()
 handleFaction faction = do
-    _ <- handleFactionFood $ faction
+    _ <- handleFactionFood faction
+    _ <- handleFactionObservations faction
+    return ()
+
+-- | Generate reports for all kinds of things faction can currently observe
+handleFactionObservations :: (BaseBackend backend ~ SqlBackend,
+    PersistStoreWrite backend, PersistQueryRead backend,
+    MonadIO m) =>
+    Entity Faction -> ReaderT backend m ()
+handleFactionObservations faction = do
+    -- observations by forces on planet
+    -- observations of space by planetary sensor arrays (SensorStation)
+    -- observations of by space forces
+    factionPlanets <- selectList [ PlanetOwnerId ==. Just (entityKey faction)] []    
+    populations <- selectList [ PlanetPopulationPlanetId <-. map entityKey factionPlanets 
+                              , PlanetPopulationPopulation >. 0 ] []
+    let populatedPlanets = filter isPopulated factionPlanets
+                            where isPopulated planet = elem (entityKey planet)
+                                    (map (planetPopulationPlanetId . entityVal) populations)
+    _ <- mapM (doPlanetObservation faction) populatedPlanets
+    return ()
+
+-- | Let forces on populated planet observe their surroundings and write reports
+doPlanetObservation :: (BaseBackend backend ~ SqlBackend,
+    PersistStoreWrite backend, PersistQueryRead backend, MonadIO m) =>
+    Entity Faction -> Entity Planet -> ReaderT backend m ()
+doPlanetObservation faction planet = do
+    time <- starDate
+    let p = entityVal planet
+    let observation = PlanetReport (entityKey planet) (Nothing) (planetStarSystemId p)
+                                   (Just $ planetName p) (Just $ planetPosition p) (Just $ planetGravity p)
+                                   (entityKey faction) (timeCurrentTime time)
+    _ <- insert observation
+    pops <- selectList [ PlanetPopulationPlanetId ==. (entityKey planet)] []
+    let popObservations = map (\pop -> PlanetPopulationReport (entityKey planet) (Just $ planetPopulationRaceId $ entityVal pop)
+                                                              (Just $ planetPopulationPopulation $ entityVal pop)
+                                                              (entityKey faction) (timeCurrentTime time)) pops
+    _ <- mapM insert popObservations
+    buildings <- selectList [ BuildingPlanetId ==. (entityKey planet)] []
+    let bObservations = map (\b -> BuildingReport (entityKey b) (entityKey planet) (Just $ buildingType (entityVal b))
+                                                  (Just $ buildingLevel (entityVal b)) (Just $ buildingConstruction (entityVal b))
+                                                  (Just $ buildingDamage (entityVal b))
+                                                  (entityKey faction) (timeCurrentTime time)) buildings
+    _ <- mapM insert bObservations
     return ()
