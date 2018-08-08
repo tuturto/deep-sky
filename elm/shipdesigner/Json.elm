b6abdb6895b4317c08343052f1a5444c25973ed4
Tuukka Turto <tuukka.turto@oktaeder.net>
2018-08-08 19:06:55 +0300

    clean code

---
+++ elm/shipdesigner/Json.elm
@@ -0,0 +1,40 @@
+module Json exposing (componentDecoder)
+
+import Json.Decode.Extra exposing ((|:))
+import Json.Decode as Decode
+import Types exposing (..)
+
+stringToSlot : String -> Decode.Decoder EquipmentSlot
+stringToSlot s =
+  case s of
+    "InnerSlot" -> Decode.succeed InnerSlot
+    "OuterSlot" -> Decode.succeed OuterSlot
+    "ArmourSlot" -> Decode.succeed ArmourSlot
+    _ -> Decode.fail "Unknown slot type"
+
+slotDecoder : Decode.Decoder EquipmentSlot
+slotDecoder =
+  Decode.string |> Decode.andThen stringToSlot
+
+stringToEqType : String -> Decode.Decoder EquipmentType
+stringToEqType s =
+  case s of
+    "BridgeEquipment" -> Decode.succeed BridgeEquipment
+    "SensorEquipment" -> Decode.succeed SensorEquipment
+    _ -> Decode.fail "Unknown component type"
+
+equipmentLevelDecoder : Decode.Decoder EquipmentLevel
+equipmentLevelDecoder =
+  Decode.succeed EquipmentLevel
+    |: (Decode.field "level" Decode.int)
+    |: (Decode.field "type" Decode.string |> Decode.andThen stringToEqType)
+
+componentDecoder : Decode.Decoder Component
+componentDecoder =
+  Decode.succeed Component
+    |: (Decode.field "id" Decode.int)
+    |: (Decode.field "name" Decode.string)
+    |: (Decode.field "description" Decode.string)
+    |: (Decode.field "weight" Decode.int)
+    |: (Decode.field "slots" <| Decode.list slotDecoder)
+    |: (Decode.field "types" <| Decode.list equipmentLevelDecoder)
--- elm/shipdesigner/Main.elm
+++ elm/shipdesigner/Main.elm
@@ -1,15 +1,15 @@
 import Html exposing (..)
 import Http
-import Json.Decode.Extra exposing ((|:))
 import Json.Decode as Decode
 import Focus exposing (..)
 import Types exposing (..)
-import Render exposing (view)
+import Render
+import Json
 
 main : Program Never Model Msg
 main =
   program { init = init
-          , view = view
+          , view = Render.view
           , update = update 
           , subscriptions = subscriptions }
 
@@ -24,51 +24,15 @@ init =
                              , requiredTypes = [ EquipmentLevel 1 BridgeEquipment ] }
                  }
       url = "/api/components"
-      cmd = Http.send AvailableComponents (Http.get url (Decode.list componentDecoder))
+      cmd = Http.send AvailableComponents (Http.get url (Decode.list Json.componentDecoder))
   in
     (newModel, cmd)
-
-stringToSlot : String -> Decode.Decoder EquipmentSlot
-stringToSlot s =
-  case s of
-    "InnerSlot" -> Decode.succeed InnerSlot
-    "OuterSlot" -> Decode.succeed OuterSlot
-    "ArmourSlot" -> Decode.succeed ArmourSlot
-    _ -> Decode.fail "Unknown slot type"
-
-slotDecoder : Decode.Decoder EquipmentSlot
-slotDecoder =
-  Decode.string |> Decode.andThen stringToSlot
-
-stringToEqType : String -> Decode.Decoder EquipmentType
-stringToEqType s =
-  case s of
-    "BridgeEquipment" -> Decode.succeed BridgeEquipment
-    "SensorEquipment" -> Decode.succeed SensorEquipment
-    _ -> Decode.fail "Unknown component type"
-
-equipmentLevelDecoder : Decode.Decoder EquipmentLevel
-equipmentLevelDecoder =
-  Decode.succeed EquipmentLevel
-    |: (Decode.field "level" Decode.int)
-    |: (Decode.field "type" Decode.string |> Decode.andThen stringToEqType)
-
-componentDecoder : Decode.Decoder Component
-componentDecoder =
-  Decode.succeed Component
-    |: (Decode.field "id" Decode.int)
-    |: (Decode.field "name" Decode.string)
-    |: (Decode.field "description" Decode.string)
-    |: (Decode.field "weight" Decode.int)
-    |: (Decode.field "slots" <| Decode.list slotDecoder)
-    |: (Decode.field "types" <| Decode.list equipmentLevelDecoder)
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
-
 
 -- UPDATE
 
