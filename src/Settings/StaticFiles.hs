f0b22a0919e30d1dabda01d59d975d556ee12285
Tuukka Turto <tuukka.turto@oktaeder.net>
2018-08-09 21:34:45 +0300

    styling

--- src/Settings/StaticFiles.hs
+++ src/Settings/StaticFiles.hs
@@ -14,7 +14,7 @@ import Yesod.Static (staticFiles)
 -- For example, to refer to @static/js/script.js@ via an identifier, you'd use:
 --
 --     js_script_js
---
+-- 
 -- If the identifier is not available, you may use:
 --
 --     StaticFile ["js", "script.js"] [] 
